500f833b791b7f3917a96a0f3a499fb9dc5be5c5
bakh0d1r <bakhodiryashinmansur@gmail.com>
2026-07-02 10:03:51 +0500

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.go
@@ -0,0 +1,9 @@
+func getConcatenation(nums []int) []int {
+    lenNums:=len(nums)
+    ans:=make([]int,2*lenNums,2*lenNums)
+    for i,v:=range nums{
+        ans[i]=v
+        ans[i+lenNums]=v
+    }
+    return ans
+}

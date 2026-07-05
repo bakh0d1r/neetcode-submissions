defd9102333713e46430590612b753fdf17c0236
bakh0d1r <bakhodiryashinmansur@gmail.com>
2026-07-05 22:09:56 +0500

    Add: 4sum - submission-4

---
+++ Data Structures & Algorithms/4sum/submission-4.go
@@ -0,0 +1,37 @@
+func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	arr := [][]int{}
+	for i := 0; i < len(nums)-3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < len(nums)-2; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			t := target - nums[i] - nums[j]
+			l := j + 1
+			r := len(nums) - 1
+			for l < r {
+				s := nums[l] + nums[r]
+				if t == s {
+					arr = append(arr, []int{nums[i], nums[j], nums[l], nums[r]})
+					l++
+					r--
+                    for l < r && nums[l] == nums[l-1] {
+					    l++
+                    }
+                    for l < r && nums[r] == nums[r+1] {
+                        r--
+                    }
+				} else if t < s {
+					r--
+				} else {
+					l++
+				}
+				
+			}
+		}
+	}
+	return arr
+}

0cf9731d9884fc1a9d3b316ab55af9fe8acdcfa9
bakh0d1r <bakhodiryashinmansur@gmail.com>
2026-07-15 21:19:48 +0500

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.go
@@ -0,0 +1,84 @@
+func simplifyPath(path string) string {
+    s:=NewStack[string]()
+    paths:=strings.Split(path,"/")
+    for i:=range paths {
+        switch paths[i] {
+            case "",".":
+                continue
+            case "..":
+                s.Pop()
+            default: 
+                s.Push(paths[i])
+        }
+    }
+    return "/" + strings.Join(s.stack,"/")
+}
+
+
+// Stack is a generic stack implementation.
+type Stack[T any] struct {
+	stack []T
+}
+
+// NewStack initializes a new stack with optional initial values.
+// Note: In Go, constructors are typically standalone functions, not methods.
+func NewStack[T any](values ...T) *Stack[T] {
+	s := &Stack[T]{
+		stack: make([]T, 0, len(values)),
+	}
+	for _, v := range values {
+		s.Push(v)
+	}
+	return s
+}
+
+// Push adds an element to the top of the stack.
+func (s *Stack[T]) Push(v T) {
+	s.stack = append(s.stack, v)
+}
+
+// Len returns the number of elements in the stack.
+func (s *Stack[T]) Len() int {
+	return len(s.stack)
+}
+
+// IsEmpty returns true if the stack contains no elements.
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.stack) == 0
+}
+
+// Last returns the top element of the stack without removing it.
+// It also returns a boolean indicating if the operation was successful.
+func (s *Stack[T]) Top() (T, bool) {
+	var zero T
+	if s.IsEmpty() {
+		return zero, false
+	}
+	return s.stack[s.LastIdx()], true
+}
+
+// LastIdx returns the index of the top element.
+func (s *Stack[T]) LastIdx() int {
+	return s.Len() - 1
+}
+
+
+// Pop removes and returns the top element of the stack.
+func (s *Stack[T]) Pop() (T, bool) {
+	var zero T
+	if s.IsEmpty() {
+		return zero, false
+	}
+
+	// Get the last element
+	pop := s.stack[s.LastIdx()]
+	// Slice off the last element
+	s.stack = s.stack[:s.LastIdx()]
+
+	return pop, true
+}
+
+// List returns the stack.
+func (s *Stack[T]) List() ([]T) {
+	return s.stack
+}

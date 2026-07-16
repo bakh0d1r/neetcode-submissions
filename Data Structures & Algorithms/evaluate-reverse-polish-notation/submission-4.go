025da43fad70b6582993eb842a712d25013dabfe
bakh0d1r <bakhodiryashinmansur@gmail.com>
2026-07-16 21:15:24 +0500

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.go
@@ -0,0 +1,105 @@
+func evalRPN(tokens []string) int {
+	s := NewStack[int]()
+	for i := range tokens {
+		switch {
+		case tokens[i] == "+":
+			n1 := s.Pop()
+			n2 := s.Pop()
+			s.Push(n2 + n1)
+		case tokens[i] == "-":
+			n1 := s.Pop()
+			n2 := s.Pop()
+			s.Push(n2 - n1)
+		case tokens[i] == "*":
+			n1 := s.Pop()
+			n2 := s.Pop()
+			s.Push(n2 * n1)
+		case tokens[i] == "/":
+			n1 := s.Pop()
+			n2 := s.Pop()
+			s.Push(n2 / n1)
+		default:
+			n := 0
+			isUs := false
+			for j := range tokens[i] {
+				if tokens[i][j] == '-' {
+					isUs = true
+					continue
+				}
+				n = n*10 + int(tokens[i][j]-'0')
+			}
+			if isUs {
+				n = -n
+			}
+			s.Push(n)
+		}
+	}
+	return s.Pop()
+}
+
+// Stack is a generic stack implementation.
+type Stack[T any] struct {
+	stack []T
+}
+
+// NewStack initializes a new stack with optional initial values.
+// Note: In Go, constructors are typically standalone functions, not methods.
+func NewStack[T any](values ...T) *Stack[T] {
+	s := &Stack[T]{
+		stack: make([]T, 0, len(values)),
+	}
+	for _, v := range values {
+		s.Push(v)
+	}
+	return s
+}
+
+// Push adds an element to the top of the stack.
+func (s *Stack[T]) Push(v T) {
+	s.stack = append(s.stack, v)
+}
+
+// Len returns the number of elements in the stack.
+func (s *Stack[T]) Len() int {
+	return len(s.stack)
+}
+
+// IsEmpty returns true if the stack contains no elements.
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.stack) == 0
+}
+
+// Last returns the top element of the stack without removing it.
+// It also returns a boolean indicating if the operation was successful.
+func (s *Stack[T]) Top() T {
+	var zero T
+	if s.IsEmpty() {
+		return zero
+	}
+	return s.stack[s.LastIdx()]
+}
+
+// LastIdx returns the index of the top element.
+func (s *Stack[T]) LastIdx() int {
+	return s.Len() - 1
+}
+
+// Pop removes and returns the top element of the stack.
+func (s *Stack[T]) Pop() T {
+	var zero T
+	if s.IsEmpty() {
+		return zero
+	}
+
+	// Get the last element
+	pop := s.stack[s.LastIdx()]
+	// Slice off the last element
+	s.stack = s.stack[:s.LastIdx()]
+
+	return pop
+}
+
+// List returns the stack.
+func (s *Stack[T]) List() []T {
+	return s.stack
+}

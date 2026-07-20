97ed1faaa073b4ed44ed42463bd1a032d20d2881
bakh0d1r <bakhodiryashinmansur@gmail.com>
2026-07-20 11:32:10 +0500

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+	l:=0
+	r:=len(nums)-1
+	for l<=r{
+		m:=(r-l)/2 + l
+		if nums[m] > target {
+			r=m-1
+		} else if nums[m] < target {
+			l=m+1
+		} else {
+			return m
+		}
+	}
+	return -1
+}
